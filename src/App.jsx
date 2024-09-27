a786c80be04469d1e5388ff48f13a32d2a31b31a
Dmytro Iovenko <iovenko@gmail.com>
2024-09-27 11:45:28 -0400

    add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Currencies from "./pages/Currencies";
 import Price from "./pages/Price";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/currencies" element={<Currencies />} />
-      <Route path="/price" element={<Price />} />
-    </Routes>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
+    </div>
   );
 }
 
