d998dd551256dc7e5e352005830c7b4530f8b5e8
Dmytro Iovenko <iovenko@gmail.com>
2024-09-27 11:41:07 -0400

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Currencies from "./pages/Currencies";
+import Price from "./pages/Price";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/currencies" element={<Currencies />} />
+      <Route path="/price" element={<Price />} />
+    </Routes>
+  );
 }
 
 export default App;
